9a56f4fcfb8012d621da08971d69e8ed42e33d17
333888956 <333888956@C-5LQJHS1.tdsb.on.ca>
2020-01-10 15:06:10 -0500

    Not finished

--- RubyMiller/src/miller/unit4/MathPlus.java
+++ RubyMiller/src/miller/unit4/MathPlus.java
@@ -22,44 +22,49 @@ public class MathPlus {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//		System.out.println("How many integers in the array?");
-		//		int amount = sc.nextInt(); //the variable amount will be used as the amount of numbers in the array
-		//		int[] userNumbers = new int[amount]; //Declaring the array
-		//		System.out.println("Enter the array");
-		//		for (int i = 0; i < amount; i++){ //This loop allows the user to input all of the numbers, and stores them in the array
-		//			userNumbers[i] = sc.nextInt(); //Storing the inputed numbers
-		//		}
-		//		System.out.println("The smallest number is " + min(userNumbers));
-		//		System.out.println("The largest number is " + max(userNumbers));
-		//		System.out.println("The sum of all the numbers is " + sum(userNumbers));
-		//		System.out.println("The average of all the numbers is " + average(userNumbers));
-		//		
-		//		System.out.println();
-		//		System.out.println("How many numbers in the array (with decimals)?");
-		//		int doubleAmount = sc.nextInt(); //the variable amount will be used as the amount of numbers in the array
-		//		double[] userDoubles = new double[doubleAmount]; //Declaring the array
-		//		System.out.println("Enter the array");
-		//		for (int i = 0; i < doubleAmount; i++){ //This loop allows the user to input all of the numbers, and stores them in the array
-		//			userDoubles[i] = sc.nextDouble(); //Storing the inputed numbers
-		//		}
-		//		System.out.println("The smallest number is " + min(userDoubles));
-		//		System.out.println("The largest number is " + max(userDoubles));
-		//		System.out.println("The sum of all the numbers is " + sum(userDoubles));
-		//		System.out.println("The average of all the numbers is " + average(userDoubles));
-		//		System.out.println("the median of the array is " + median(userDoubles));
-		//		System.out.println("The mode of the array is " + mode(userDoubles));
+		System.out.println("How many integers in the array?");
+		int amount = sc.nextInt(); //the variable amount will be used as the amount of numbers in the array
+		int[] userNumbers = new int[amount]; //Declaring the array
+		System.out.println("Enter the array");
+		for (int i = 0; i < amount; i++){ //This loop allows the user to input all of the numbers, and stores them in the array
+			userNumbers[i] = sc.nextInt(); //Storing the inputed numbers
+		}
+		System.out.println("The smallest number is " + min(userNumbers));
+		System.out.println("The largest number is " + max(userNumbers));
+		System.out.println("The sum of all the numbers is " + sum(userNumbers));
+		System.out.println("The average of all the numbers is " + average(userNumbers));
+
+		System.out.println();
+		System.out.println("How many numbers in the array (with decimals)?");
+		int doubleAmount = sc.nextInt(); //the variable amount will be used as the amount of numbers in the array
+		double[] userDoubles = new double[doubleAmount]; //Declaring the array
+		System.out.println("Enter the array");
+		for (int i = 0; i < doubleAmount; i++){ //This loop allows the user to input all of the numbers, and stores them in the array
+			userDoubles[i] = sc.nextDouble(); //Storing the inputed numbers
+		}
+		System.out.println("The smallest number is " + min(userDoubles));
+		System.out.println("The largest number is " + max(userDoubles));
+		System.out.println("The sum of all the numbers is " + sum(userDoubles));
+		System.out.println("The average of all the numbers is " + average(userDoubles));
+		System.out.println("the median of the array is " + median(userDoubles));
+		System.out.println("The mode of the array is " + mode(userDoubles));
 
 		System.out.println("Enter an integer");
 		int number = sc.nextInt(); //The integer that will be the parameter of the next few methods.
 		System.out.print(number + " is ");
-//		if (prime(number) == true) { //Calling the prime method with the inputed integer, if it's true it's prime, if false, it isn't prime.
-//			System.out.println("a prime number.");
-//		}
-//		else {
-//			System.out.println("not a prime number.");}
-//		}
+		if (prime(number) == true) { //Calling the prime method with the inputed integer, if it's true it's prime, if false, it isn't prime.
+			System.out.println("a prime number.");
+		}
+		else {
+			System.out.println("not a prime number.");
+		}
 		System.out.println("The factorial of " + number + " is " + factorial(number));
 		System.out.println(number + " has " + numOfFactors(number) + " factors");
+		System.out.print(number + "'s factors are ");
+		int[] print = factors(number);
+		for (int i = 0; i < numOfFactors(number); i++) {
+			System.out.print(print[i] + " ");
+		}
 	}
 
 	/**
@@ -164,11 +169,13 @@ public class MathPlus {
 	 * @return the median of all the numbers (doubles) in the given array.
 	 */
 	public static double median(double[]numbers) {
-		//Make the array in increaing order???
+		Arrays.sort(numbers);
 		if (numbers.length % 2 == 0) { //If there is an even amount of numbers in the array.
-			return (numbers[numbers.length / 2 - 1] + numbers[(numbers.length / 2)] ) / 2; //Finding the 2 numbers in the middle, and dividing the sum of them by 2.
+			return ((numbers[numbers.length / 2 - 1]) + (numbers[(numbers.length / 2)] )) / 2; //Finding the 2 numbers in the middle, and dividing the sum of them by 2.
 		}
-		return numbers[numbers.length / 2]; //If there is an odd amount of numbers in the array, find the middle one
+		else {
+		return (numbers[numbers.length / 2]); //If there is an odd amount of numbers in the array, find the middle one
+		}
 	}
 
 	/**
@@ -178,14 +185,14 @@ public class MathPlus {
 	 */
 	public static double mode(double[]numbers) {
 		int counter2 = 0;
-		double mode = numbers[0];
-		for (int i = 0; i < numbers.length; i++) {
+		double mode = numbers[0]; //The mode will start as the first number, then change after.
+		for (int i = 0; i < numbers.length; i++) { //The loop will stop when it has gone through all of the numbers in the array.
 			double value = numbers[i];
 			int counter = 1;
-			for (int j = 0; j < numbers.length; j++) {
-				if (numbers[j] == value) {
+			for (int j = 0; j < numbers.length; j++) { //This loop ends when it goes through all the numbers.
+				if (numbers[j] == value) { //If the new number is the same as the previous number, 1 adds to the counter.
 					counter++;
-					if (counter > counter2) {
+					if (counter > counter2) { //Whichever number there is more of, becomes the mode.
 						mode =  value;
 						counter2 = counter;
 					}
@@ -200,20 +207,22 @@ public class MathPlus {
 	 * @param number - the number that the program calculates if it's prime or not. <br>
 	 * @return true if the number is prime, or false if it's not
 	 */
-//	public static boolean prime(int number) {
-//		int counter = 1;
-//		while (counter < Math.sqrt(number)) {
-//			counter++;
-//			if (number % counter != 0) {
-//				return true;
-//			}
-//			else {
-//				return false;
-//				break;
-//			}
-//		}
-//		return false;
-//	}
+	public static boolean prime(int number) { 
+		int counter = 1;                      
+		while (counter < Math.sqrt(number)) {
+			counter++;
+			if (number == 1 || number == 0){ // Since the counter starts at 2, 1 is not included, but 1 is a prime number
+				return false;
+			}
+			else if ((number == 2) || (number % counter != 0)){
+				return true;
+			}
+			else {
+				break;
+			}
+		}
+		return false;
+	}
 	
 	/**
 	 * This method calculates the factorial of the given number. <br>
@@ -244,8 +253,22 @@ public class MathPlus {
 
 	}
 	
-	public static int[] factors(int number) {
-		
+	/**
+	 * This method finds all of the given number's factors. <br>
+	 * @param number - the number that's factors are found. <br>
+	 * @return an array that includes all of the factors the given number has.
+	 */
+	public static int[] factors(int number) {          
+		int[] factors = new int[numOfFactors(number)]; 
+		int counter = 0;
+		for (int i = 1; i <= number; i++) {
+			int factor = number / i;
+			if (number % i == 0) {
+				factors[counter] = factor;
+				counter++;
+			}
+		}
+		return factors;
 	}
 }
 
